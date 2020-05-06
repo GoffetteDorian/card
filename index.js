0425e453168a64fc28c9e04a07f7869f1802eb72
GoffetteDorian <goffettedorian@gmail.com>
2020-05-06 16:54:00 +0200

    boxen testing

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 #!/usr/bin/env node
+
+const boxen = require("boxen");
+
+console.log(
+  boxen("unicorn", {
+    padding: 1,
+    borderStyle: "double",
+    borderColor: "redBright",
+    backgroundColor: "yellow",
+  })
+);
